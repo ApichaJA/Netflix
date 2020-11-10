fa457c6fb6988f3be3ca29d5f536a27ef87adfa2
ApichaJA <Junyatanakorn@gmail.com>
2020-11-10 22:10:54 +0700

    add rating

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,14 @@
 <div class="main-browse">
 
   <div class="videoWrapper">
+    <div class="feach-video">
     <video class="top-main-motion" autoplay loop>
       <source :src="topvideo" type="video/mp4">
     </video>
+    <div class="site-right-info">
+      <div class="rating-main-box"><span>16+</span></div>
+    </div>
+    </div>
   </div>
 </div>
 </template>
@@ -17,7 +22,8 @@ export default {
   },
   computed: {
   topvideo () {
-    return require('@/assets/videos/wywslp.mp4')
+    var movieName = "wywslp"
+    return require('@/assets/videos/' + movieName + '.mp4')
   }
 }
 }
@@ -29,6 +35,24 @@ export default {
   z-index: 1;
   position: absolute;
   top: -7%;
+}
+
+.feach-video{
+  position: relative;
+}
+.site-right-info{
+  position: absolute;
+  right: 0;
+  bottom: 33vh;
+}
+
+.rating-main-box{
+  border-left: solid 3px #dcdcdc;
+  padding: .5vw 3.5vw .5vw .8vw;
+  height: 2.4vw;
+  background-color: rgba(0, 0, 0, .4);
+  font-size: 1.1vw;
+  color: #fff;
 }
 
 .top-main-motion {
